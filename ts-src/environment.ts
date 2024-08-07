04e3c97ca6793f589b1738510fe3612446266e0d
Tarun-Jaikishan <tarunjaikishanpk@gmail.com>
2024-08-07 21:52:46 +0530

    changes

--- ts-src/environment.ts
+++ ts-src/environment.ts
@@ -4,5 +4,4 @@ dotenv.config();
 
 export const envPort = process.env.PORT as string;
 export const envNodeEnv = process.env.NODE_ENV as string;
-export const envJwtKey = process.env.JWT_KEY as string;
 export const envSessionKey = process.env.SESSION_KEY as string;
